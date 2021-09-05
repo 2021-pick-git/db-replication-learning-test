3e11ca10f326674ee70eab8f575ed590661f40c7
YunTroubleshooter <cairofaith@gmail.com>
2021-09-05 14:44:44 +0900

    chore: project setting

---
+++ src/main/java/com/pickgit/dbreplicationlearningtest/DbReplicationLearningTestApplication.java
@@ -0,0 +1,13 @@
+package com.pickgit.dbreplicationlearningtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbReplicationLearningTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbReplicationLearningTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pickgit/dbreplicationlearningtest/DbReplicationLearningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickgit.dbreplicationlearningtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbReplicationLearningTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
